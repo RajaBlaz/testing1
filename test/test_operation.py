6466befac7e3f9b2e7ca55fa4017b300d762ccb1
Raja Blaz <furiousbird8@gmail.com>
2024-09-05 22:44:53 +0500

    operations

---
+++ test/test_operation.py
@@ -0,0 +1,9 @@
+from src.math_operation import add,sub
+
+assert add(1, 2) == 3
+assert add(1, -2) == -1
+assert add(5, 2) == 7
+
+assert sub(1, 2) == -1
+assert sub(1, -2) == 3
+assert sub(5, 2) == 3
